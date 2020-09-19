883a84e982a0dc5676f9d1117dd8c51ee2c9e9a8
Christine Than <krispthan@gmail>
2020-09-19 16:55:26 -0700

    initial local commit created homepage, directory and menu-item

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Homepage from './pages/homepage/homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Homepage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
